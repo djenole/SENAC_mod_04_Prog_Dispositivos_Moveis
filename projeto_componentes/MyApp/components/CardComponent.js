1b1d584a591d8004bc9a36801478ae6e3c43da13
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-28 21:24:04 -0300

    foi retirado o stylesheet para deixar o component apenas com card material

--- projeto_componentes/MyApp/components/CardComponent.js
+++ projeto_componentes/MyApp/components/CardComponent.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import {StyleSheet, Section } from 'react-native';
+import { Avatar, Button, Card, Title, Paragraph, DefaultTheme,  } from 'react-native-paper';
 
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 const CardComponent = () => (
    
-    <Card>
+
+
+<Card>
     <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
     <Card.Content>
       <Title>Card title</Title>
@@ -17,24 +19,14 @@ const CardComponent = () => (
       <Button>Cancel</Button>
       <Button>Ok</Button>
     </Card.Actions>
-  </Card>
+</Card>
+
+
+
+  
+ 
+    
     
 );
 
 export default CardComponent;
-
-const styles = StyleSheet.create({
-    card: {
-      marginTop:100,
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center' 
-    
-    
-    },
-    textSize: {
-      
-     
-    }
-  });
